9e317ca4b74da721fd89718c09574dfaa8f936f5
Jonathan Lor <jonathan.r.lor@gmail.com>
2026-02-07 11:33:50 -0600

    adding directories

---
+++ app/ios/Logan/Logan/LoganApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoganApp.swift
+//  Logan
+//
+//  Created by jonathan lor on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LoganApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app/ios/Logan/LoganTests/LoganTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoganTests.swift
+//  LoganTests
+//
+//  Created by jonathan lor on 2/7/26.
+//
+
+import Testing
+@testable import Logan
+
+struct LoganTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
